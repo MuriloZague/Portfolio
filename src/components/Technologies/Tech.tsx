48ac053701d99f3f740cd2afdab0c679884ffb5e
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-09-10 11:02:19 -0300

    first commit

--- src/components/Technologies/Tech.tsx
+++ src/components/Technologies/Tech.tsx
@@ -6,7 +6,7 @@ type TechProps = {
 export default function Tech({name, icons}: TechProps) {
  return (
   <section className="w-full max-w-fit flex flex-row justify-center mt-8 cursor-default">
-    <div className="mt-5">
+    <div className="mt-3">
     <p className="mb-3">// {name}:</p>
     <div className='flex gap-3'>
         {icons.map((icon, index) => (
